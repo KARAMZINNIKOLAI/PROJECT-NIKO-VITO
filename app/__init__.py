0ef44ce31717316bc3cd4accfa4250658599ce3b
Николай <karamzinkolya@gmail.com>
2023-05-29 17:02:44 +0900

    что-то

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
 
-app = Flask(__name__)
+from app import views
 
-@app.route("/")
-def index_page():
-    return "<p>Hello, World!</p>"
+from app.database import db, migrate
+
+
+def create_app():
+    app = Flask(__name__)
+    app.add_url_rule("/", view_func=views.index_page)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    return app
