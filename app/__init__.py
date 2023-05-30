51f0980450b15c8eb234c2edbf6ce9bfa36bbc4a
Николай <karamzinkolya@gmail.com>
2023-05-30 18:20:47 +0900

    цвет поменял, админку добавил, файл базы данных исправил

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,25 @@
 from flask import Flask
 
-from app import views
+from .models import db, migrate
+from . import views
+
+from .admin import admin
 
 from app.database import db, migrate
 
 
 def create_app():
     app = Flask(__name__)
+
+    app.config["SECRET_KEY"] = "secret"
+
+    admin.init_app(app)
+
+    #функция представления
     app.add_url_rule("/", view_func=views.index_page)
+
+    app.add_url_rule("/login/", view_func=views.login_page, methods=["GET", "POST"])
+    app.add_url_rule("/logout/", view_func=views.logout)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
     db.init_app(app)
